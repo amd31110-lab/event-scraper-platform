06129a92e0a741369933a2a79fbcfe82b0b250dd
amd31110-lab <amd31110@gmail.com>
2026-03-06 12:19:24 +0530

    Add root App component with Home page

---
+++ server/server/server/routes/server/scraper/client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Home from "./pages/Home";
+
+/*
+  Root component of the application
+*/
+
+function App() {
+
+  return (
+    <div>
+
+      <Home />
+
+    </div>
+  );
+
+}
+
+export default App;
